73275c21ac07478e9fa66169a0f803baf9236c16
Paulina Gacek <paulina.gacek.pl@gmail.com>
2021-07-21 12:07:22 +0200

    Solution to FrogRiver added

---
+++ FrogRiverOne.cpp
@@ -0,0 +1,16 @@
+// Frog River
+// Assessed time complexity - O(N)
+// Scored for 100%
+
+#include <set>
+int solution(int X, vector<int> &A) {
+    std::set<size_t> leaves;
+    for(size_t i = 0; i < A.size(); ++i){
+        if(A.size()-i < X-leaves.size()) //it will be impossible to reach all needed values
+            return -1;
+        leaves.insert(A[i]);
+        if( static_cast<int> (leaves.size()) == (X))
+            return static_cast<int> (i);
+    }
+    return -1;
+}
